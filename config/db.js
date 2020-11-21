fccc59f8ddbb7284a12fb270edcc01eae3395ecf
tanushreeneelam <tanushreeneelam28@gmail.com>
2020-11-22 00:44:47 +0530

    no .env

--- config/db.js
+++ config/db.js
@@ -1,7 +1,8 @@
 // require('dotenv').config()
 require('dotenv').config({ path: require('find-config')('.env') });
 const mongoose = require('mongoose')
-const url= process.env.URL
+// const url= process.env.URL
+const url= "mongodb+srv://Tanushree:ILoveRaffa28@rsvp.fh3wk.mongodb.net/todo-db?retryWrites=true&w=majority"
 
 const connectDB = async () => {
     try{
